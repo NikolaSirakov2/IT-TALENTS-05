0917df0d6ee1aa068790eba42fb0bd4e1c46b603
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2022-12-20 16:22:39 +0200

    Create 03.checkCh.js

---
+++ 03.checkCh.js
@@ -0,0 +1,13 @@
+let arr = ["arr", "arr", "cha", "arr"];
+let have = false;
+
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] === "ch") {
+    have = true;
+    console.log(i);
+  }
+}
+
+if (have === false) {
+  console.log(-1);
+}
