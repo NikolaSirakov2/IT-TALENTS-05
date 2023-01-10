034802c3c22025239a77450bbd2206b7c288a4f4
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2023-01-10 15:48:42 +0200

    Update funcTest.js

--- 23december.js/funcTest.js
+++ 23december.js/funcTest.js
@@ -8,10 +8,10 @@
 
 // a();
 
-function one(a, b, c) {
-  return function (a, b, c) {
-    return a + b + c;
-  };
-}
+// function one(a, b, c) {
+//   return function (a, b, c) {
+//     return a + b + c;
+//   };
+// }
 
-one(1, 2, 3);
+// one(1, 2, 3);
